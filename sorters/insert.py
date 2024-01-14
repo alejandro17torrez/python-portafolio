439e45ca9df655d7d38906f439cc3f019e09df96
Alejandro Torrez <ciberthingscomputinglearning@outlook.com>
2024-01-14 17:05:18 -0300

    remove files

--- sorters/insert.py
+++
@@ -1,9 +0,0 @@
-def insert(list: list) -> list:
-    """sorting method by insert"""
-
-    for item in range(1, len(list) + 1):
-        k = item - 1
-        while (k > 0) and (list[k] < list[k - 1]):
-            list[k], list[k - 1] = list[k - 1], list[k]
-            k -= 1
-    return list
